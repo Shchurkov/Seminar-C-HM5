842b128924711aadcc42540c85c00523511fb24a
Shchurkov <mixxars@gmail.com>
2023-01-30 13:22:16 +0700

    Решена задача 38

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+float[] array = new float[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+float max = array[0];
+float min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    
+    if (array[i] < min)
+            min = array[i];
+    if (array[i] > max)
+            max = array[i];
+}
+Console.WriteLine ($"Максимальное число {max}, минимальное число {min}");
